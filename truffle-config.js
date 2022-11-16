37bcf772327ea25bfd26e17309ff004d3c8cc387
prasanjit <prasanjitdutta45@gmail.com>
2022-11-17 02:55:14 +0530

    cleaned 2

--- truffle-config.js
+++ truffle-config.js
@@ -2,21 +2,14 @@ const dotenv = require("dotenv").config();
 const HdWalletProvider = require("@truffle/hdwallet-provider");
 
 const MNEMONIC = process.env.MNEMONIC;
-const BSC_TESTNET = new HdWalletProvider(MNEMONIC, process.env.BSC_PROVIDER);
 
 
 module.exports = {
   networks: {
     development: {
-     host: "127.0.0.1",
-     port: 7545,     
-     network_id: "*",   
-    },
-    
-    bsc_testnet: {
-      provider: BSC_TESTNET,
-      network_id: 97,
-      skipDryRun: true
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
     }
   },
   compilers: {
